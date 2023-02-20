ef39678cbbe50fe37bb778f6905800ff145f2ae6
lucianogomes02 <luciano@pontotel.com.br>
2023-02-19 21:10:14 -0300

    feat(escola/models.py): inicia o app escola e cria os modelos Aluno, Curso e suas migrações

---
+++ escola/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from enum import Enum
+
+
+class Nivel(Enum):
+    B: str = ("Básico",)
+    I: str = "Intermediário"
+    A: str = "Avançado"
+
+    @classmethod
+    def opcoes(cls):
+        return (
+            ("B", cls.B),
+            ("I", cls.I),
+            ("A", cls.A),
+        )
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+    codigo_curso = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100)
+    nivel = models.CharField(
+        max_length=1, choices=Nivel.opcoes(), blank=False, null=False, default="B"
+    )
+
+    def __str__(self):
+        return self.descricao
