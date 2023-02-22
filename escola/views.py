933a781cca614a2ddeabe63d7292a2b9fc6fa1ab
lucianogomes02 <luciano@pontotel.com.br>
2023-02-22 00:37:46 -0300

    feat(escola/views.py): implementa viewset de AlunosViewSet e CursosViewSet

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializers import AlunoSerializer, CursoSerializer
 
-# Create your views here.
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """API de Alunos"""
+
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """API de Cursos"""
+
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
