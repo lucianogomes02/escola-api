ff2231553690974ed5a0a4737eac783dfa52c717
lucianogomes02 <luciano@pontotel.com.br>
2023-02-24 19:06:03 -0300

    feat(setup/urls.py) : implementa url da documentação da API com o Swagger

--- setup/urls.py
+++ setup/urls.py
@@ -7,9 +7,25 @@ from escola.views import (
     ListaMatriculasAluno,
     ListaAlunosDoCurso,
 )
-from rest_framework import routers
+from rest_framework import routers, permissions
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Escola-API",
+        default_version="v1",
+        description="API de Registro de Alunos, Cursos e Matrículas dos Alunos nos Cursos",
+        terms_of_service="#",
+        contact=openapi.Contact(email="lucianogvda02@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 
 router = routers.DefaultRouter()
@@ -24,4 +40,9 @@ urlpatterns = [
     path("", include(router.urls)),
     path("alunos/<int:pk>/matriculas", ListaMatriculasAluno.as_view()),
     path("cursos/<int:pk>/matriculas", ListaAlunosDoCurso.as_view()),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
