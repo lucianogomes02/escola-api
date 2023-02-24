8f9374d329cacbfe33ac671ab06555ad53ca3510
lucianogomes02 <luciano@pontotel.com.br>
2023-02-24 09:16:26 -0300

    test(escola/tests/tests_cursos.py): implementa testes de GET e POST no CursosTestCase

---
+++ escola/tests/tests_cursos.py
@@ -0,0 +1,33 @@
+from rest_framework.test import APITestCase
+from escola.models import Curso
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+
+
+class CursosTestCase(APITestCase):
+    def setUp(self):
+        self.list_url = reverse("Cursos-list")
+        self.user = User.objects.create_superuser(
+            email="asdf@gmail.com",
+            password="hiwa_asdf",
+            username="smile as we go ahead",
+        )
+        self.client.force_authenticate(self.user)
+        self.primeiro_curso = Curso.objects.create(
+            codigo_curso="CTT1", descricao="Curso Teste 1", nivel="B"
+        )
+        self.segundo_curso = Curso.objects.create(
+            codigo_curso="CTT2", descricao="Curso Teste 2", nivel="I"
+        )
+
+    def test_get_cursos(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_curso(self):
+        data = {"codigo_curso": "CTT3", "descricao": "Curso Teste 3", "nivel": "A"}
+
+        response = self.client.post(self.list_url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
